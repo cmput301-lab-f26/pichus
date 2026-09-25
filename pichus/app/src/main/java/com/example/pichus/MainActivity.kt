e0acba9ceec31b56915a07080da504ae88a7aa72
wesweee <159965204+wesweee@users.noreply.github.com>
2026-09-24 23:44:26 -0600

    Change default color of Shape class from green to red

--- pichus/app/src/main/java/com/example/pichus/MainActivity.kt
+++ pichus/app/src/main/java/com/example/pichus/MainActivity.kt
@@ -39,6 +39,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 
-open class Shape(x: Int, y: Int, color: String = "green") {
+open class Shape(x: Int, y: Int, color: String = "red") {
 }
 
